62c76091197d3b033ac459740a5ea7aeda15afa9
Corey <coreyhamat@gmail.com>
2021-10-19 17:11:41 -0400

    finished wireframe and most styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,60 +1,95 @@
 const portfolio = {}
-portfolio.techStack = [
-  {
-    name:"HTML5",
-    class:`devicon-html5-plain-wordmark`
-  },
-  {
-    name:'CSS3',
-    class:'devicon-css3-plain-wordmark'
-  },
-  {
-    name:'JavaScript',
-    class:'devicon-javascript-plain'
-  },
-  {
-    name:'JQuery',
-    class:'devicon-jquery-plain-wordmark'
-  },
-  {
-    name:"react",
-    class:'devicon-react-original-wordmark'
-  },
-  {
-    name:'GitHub',
-    class:'devicon-github-original-wordmark'
-  },
-  {
-    name:'Git',
-    class:'devicon-git-plain-wordmark'
-  },
-  {
-    name:'NodeJs',
-    class:'devicon-nodejs-plain'
-  },
-  {
-    name:'Firebase',
-    class:'devicon-firebase-plain-wordmark'
-  }
-]
 
 portfolio.init = () => {
+  AOS.init();
   portfolio.addStack();
+  portfolio.addProjects();
+  // portfolio.navScroll();
+  // portfolio.arrowScroll();
 }
 
+// portfolio.arrowScroll = () => {
+//   const scrollArrow = document.querySelector(`.startScroll a[href^="#"]`)
+//   scrollArrow.onClick(function(e) {
+//     let hash = this.getAttribute('href')
+//     let target = document.querySelector(hash);
+//     let navOffset = 100;
+//     let destPosition = target.offsetTop;
+//     let offsetPosition = destPosition - navOffset
+
+//     window.scrollTo({
+//       top:offsetPosition,
+//       behavior:'smooth'
+//     });
+//   })
+// }
+
+// portfolio.navScroll = () => {
+//   const navLinks = document.querySelectorAll('nav a[href^="#"]')
+//   navLinks.forEach((anchor) => {
+//     anchor.onClick = function(e) {
+//       e.preventDefault();
+//       let hash = this.getAttribute('href')
+//       let target = document.querySelector(hash)
+//       let navOffset = 100;
+//       let destPosition = target.offsetTop;
+//       let offsetPosition = destPosition - navOffset;
+
+//       window.scrollTo({
+//         top:offsetPosition,
+//         behavior:'smooth',
+//       });
+//     };
+//   });
+// }
+
+
 portfolio.addStack = () => {
-  const gallery = document.querySelector(".skillGallery")
-  const listElement = document.createElement("ul")
-  portfolio.techStack.forEach((skill) => {
+  const gallery = document.querySelector(".skillList")
+  skillArray.forEach((skill) => {
     const listItem = document.createElement("li")
-    console.log(skill)
+    listItem.setAttribute('data-aos', 'flip-up')
     listItem.innerHTML = `
-    <i class="${skill.class}"></i>
-    <p class="sr-only">${skill.name}</p>
+    <div class="skillContainer">
+      <i class="${skill.class}"></i>
+      <p>${skill.name}</p>
+    </div>
     `
-    listElement.appendChild(listItem)
+    gallery.appendChild(listItem)
   })
-  gallery.appendChild(listElement)
+}
+
+portfolio.addProjects = () => {
+  const gallery = document.querySelector(".projectList")
+  projectArray.forEach((project) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add('project')
+    listItem.setAttribute("data-aos", "zoom-in")
+    const divElement = document.createElement('div')
+    divElement.classList.add("projectContainer")
+    divElement.innerHTML =  `
+        <img src="${project.src}" alt="${project.name} screenshot" class="screenshot">
+        <div class="projectText">
+          <h3>${project.name}</h3>
+          <div class="links">
+            <a href="${project.repo}" class="link">GitHub Repo </a>
+            <a href="${project.live}" class="link">Live</a>
+          </div>
+          <p>${project.description}</p>
+        </div>
+    `
+    listItem.appendChild(divElement)
+    const langList = document.createElement('ul')
+    langList.classList.add("languages")
+    project.languages.forEach((language) => {
+      const langLi = document.createElement('li')
+      langLi.aos = "flip-up"
+      langLi.innerHTML = `<p>${language}</p>`
+      langList.appendChild(langLi)
+      divElement.appendChild(langList)
+    })
+    gallery.appendChild(listItem)
+  })
 }
 
 portfolio.init()
---
+++ scripts/arrays.js
@@ -0,0 +1,102 @@
+const projectArray = [
+  {
+    name:'The Nygma Machine',
+    languages:[
+      "React", 
+      "Firebase", 
+      "JSX",
+      "SASS"
+    ],
+    src:`./assets/nygmaPreview.jpg`,
+    description:"A randomly generated maze game where, when completed, the user receives a piece of advice (provided by the AdviceSlip API) based on a keyword they select before starting the game.",
+    repo:`https://github.com/nygma-machine/project-4`,
+    live:`https://nygma.netlify.app`
+  },
+  {
+    name:'The Digital Cinephile',
+    languages:[
+      "JavaScript",
+      "HTML",
+      "SASS"
+    ],
+    description:`A Movie/TV Show recommendation app, developed through paired programming & built with the TMDB API. A user searches for a movie and/or TV show, then the site  displays a gallery of recommended movies/TV Shows based on their input. The website uses multiple API calls to display each recommendation's poster, official trailer (if available), user rating, and plot overview.`,
+    src:'./assets/cinephilePreview.jpg',
+    repo:`http://github.com/cohort36-Bootcamp-Project-02/theDigitalCinephile/`,
+    live:`https://theDigitalCinephile.netlify.app`
+  },
+  {
+    name:'The Perfect Playlist Procurement Program',
+    languages:[
+      "React",
+      "SASS",
+      "Firebase"
+    ],
+    src:`./assets/playlistPreview.jpg`,
+    description:`A playlist generation app, built using the Deezer API. Users select an artist/band, then a random song of theirs is added to a global 'playlist'. Users can search for bands either by genre, or by searching for their name.`,
+    repo:`https://github.com/chamat96/corey-hamat-project-03`,
+    live:`https://perfect-playlist-procurement.netlify.app`
+  },
+  {
+    name:'Planted',
+    languages:[
+      "HTML",
+      "SASS",
+      "CSS"
+    ],
+    src:`./assets/plantedPreview.jpg`,
+    description:"A Multi-Page HTML/SCSS website that was designed based on a client brief/styleguide",
+    repo:`https://github.com/CHamat96/coreyHamatProject01`,
+    live:'https://chamat96.github.io/coreyHamatProject01/index.html'
+  },
+]
+
+const skillArray = [
+  {
+    name:"HTML5",
+    class:'devicon-html5-plain'
+  },
+  {
+    name:'CSS3',
+    class:'devicon-css3-plain'
+  },
+  {
+    name:'SASS',
+    class:'devicon-sass-original'
+  },
+  {
+    name:'JavaScript',
+    class:'devicon-javascript-plain'
+  },
+  {
+    name:'JQuery',
+    class:'devicon-jquery-plain'
+  },
+  {
+    name:"React",
+    class:'devicon-react-original'
+  },
+  {
+    name:'GitHub',
+    class:'devicon-github-original'
+  },
+  {
+    name:'Git',
+    class:'devicon-git-plain'
+  },
+  {
+    name:'NodeJs',
+    class:'devicon-nodejs-plain'
+  },
+  {
+    name:'Firebase',
+    class:'devicon-firebase-plain'
+  },
+  {
+    name:'Accessibility',
+    class:'fas fa-universal-access'
+  },
+  {
+    name:'RESTful APIs',
+    class:'fas fa-cogs'
+  }
+]
