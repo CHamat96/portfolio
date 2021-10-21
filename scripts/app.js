5f1f33d05a563667847f7555ec19d65cc2980d3d
Corey <coreyhamat@gmail.com>
2021-10-21 13:10:16 -0400

    finished styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,12 @@
 const portfolio = {}
 
 portfolio.init = () => {
-  portfolio.addStack();
-  portfolio.addProjects();
+  portfolio.addItems();
   portfolio.navScroll();
   portfolio.calendly();
 }
 
-// 'smooth scroll' method
+// use a 'smooth scroll' whenever user clicks on an 'anchor' that leads to a certain section of the page
 portfolio.navScroll = () => {
   const navElements = document.querySelectorAll('.navScroll')
   navElements.forEach((anchor) => {
@@ -26,52 +25,48 @@ portfolio.navScroll = () => {
   })
 }
 
-// adjusting default settings for AOS package
+// adjusting default settings for Animate-on-Scroll package
 AOS.init({
   duration:700,
   delay:100,
 })
 
-
-// Display objects from 'tech stack' array on webpage
-portfolio.addStack = () => {
-  const gallery = document.querySelector(".skillList")
-  skillArray.forEach((skill) => {
-    const listItem = document.createElement("li")
-    listItem.setAttribute('data-aos', 'flip-up')
-    listItem.innerHTML = `
-    <div class="skillContainer">
-      <i class="${skill.class}"></i>
-      <p>${skill.name}</p>
-    </div>
-    `
-    gallery.appendChild(listItem)
-  })
-}
-
-// Display objects from 'projects' array onto webpage
-portfolio.addProjects = () => {
-  const gallery = document.querySelector(".projectList")
+// Display objects from 'projects'  and 'skills' array onto webpage
+portfolio.addItems = () => {
+  const projectGallery = document.querySelector(".projectList")
   projectArray.forEach((project) => {
     const listItem = document.createElement("li");
     listItem.classList.add('project')
     listItem.setAttribute("data-aos", "zoom-in")
+
     const divElement = document.createElement('div')
     divElement.classList.add("projectContainer")
     divElement.innerHTML =  `
-        <img src="${project.src}" alt="${project.name} screenshot" class="screenshot">
-        <div class="projectText">
-          <h3>${project.name}</h3>
-          <div class="links">
-            <a href="${project.repo}" class="link">View Code </a>
-            <a href="${project.live}" class="link">View Live</a>
-          </div>
-          <p>${project.description}</p>
+      <img src="${project.src}" alt="${project.name} screenshot">
+      <div class="projectText">
+        <h3>${project.name}</h3>
+        <div class="links">
+          <a href="${project.repo}" class="link">View Code </a>
+          <a href="${project.live}" class="link">View Live</a>
         </div>
+        <p>${project.description}</p>
+      </div>
     `
     listItem.appendChild(divElement)
-    gallery.appendChild(listItem)
+    projectGallery.appendChild(listItem)
   })
+  const skillGallery = document.querySelector(".skillList")
+  techStack.forEach((skill) => {
+      const listItem = document.createElement("li")
+      listItem.setAttribute('data-aos', 'flip-up')
+      listItem.innerHTML = `
+      <div class="skillContainer">
+        <i class="${skill.class}"></i>
+        <p>${skill.name}</p>
+      </div>
+      `
+      skillGallery.appendChild(listItem)
+    })
 }
 
 
--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,68 +1,4 @@
-const projectArray = [
-  {
-    name:'The Nygma Machine',
-    languages:[
-      "React", 
-      "Firebase", 
-      "HTML5",
-      "SCSS",
-      "CSS3",
-      "JavaScript",
-      "Firebase",
-      "Group Collab",
-      "API"
-    ],
-    src:`./assets/nygmaPreview.jpg`,
-    description:"A randomly generated maze game where, when completed, the user receives a piece of advice (provided by the AdviceSlip API) based on a keyword they select before starting the game.",
-    repo:`https://github.com/nygma-machine/project-4`,
-    live:`https://nygma.netlify.app`
-  },
-  {
-    name:'The Digital Cinephile',
-    languages:[
-      "JavaScript",
-      "HTML5",
-      "SCSS",
-      "CSS3",
-      "Paired Programming",
-      "API"
-    ],
-    description:`A Movie/TV Show recommendation app, developed through paired programming & built with the TMDB API. A user searches for a movie and/or TV show, then the site  displays a gallery of recommended movies/TV Shows based on their input. The website uses multiple API calls to display each recommendation's poster, official trailer (if available), user rating, and plot overview.`,
-    src:'./assets/cinephilePreview.jpg',
-    repo:`http://github.com/cohort36-Bootcamp-Project-02/theDigitalCinephile/`,
-    live:`https://theDigitalCinephile.netlify.app`
-  },
-  {
-    name:'The Perfect Playlist Procurement Program',
-    languages:[
-      "React",
-      "Firebase",
-      "JavaScript",
-      "SCSS",
-      "HTML5",
-      "CSS3",
-      "API"
-    ],
-    src:`./assets/playlistPreview.jpg`,
-    description:`A playlist generation app, built using the Deezer API. Users select an artist/band, then a random song of theirs is added to a global 'playlist'. Users can search for bands either by genre, or by searching for their name.`,
-    repo:`https://github.com/chamat96/corey-hamat-project-03`,
-    live:`https://perfect-playlist-procurement.netlify.app`
-  },
-  {
-    name:'Planted',
-    languages:[
-      "HTML5",
-      "CSS3",
-      "SCSS"
-    ],
-    src:`./assets/plantedPreview.jpg`,
-    description:"A Multi-Page HTML/SCSS website that was designed based on a client brief/styleguide",
-    repo:`https://github.com/CHamat96/coreyHamatProject01`,
-    live:'https://chamat96.github.io/coreyHamatProject01/index.html'
-  },
-]
-
-const skillArray = [
+const techStack = [
   {
     name:"HTML5",
     class:'devicon-html5-plain'
@@ -96,14 +32,6 @@ const skillArray = [
     class:'devicon-git-plain'
   },
   {
-    name:'NodeJs',
-    class:'devicon-nodejs-plain'
-  },
-  {
-    name:'npm',
-    class:'devicon-npm-original-wordmark'
-  },
-  {
     name:'Firebase',
     class:'devicon-firebase-plain'
   },
@@ -127,4 +55,105 @@ const skillArray = [
     name:"Adobe Creative Cloud",
     class:'fas fa-cloud'
   }
+]
+
+const projectArray = [
+  {
+    name:'The Nygma Machine',
+    languages:[
+      {
+        name:"HTML5",
+        class:'devicon-html5-plain'
+      },
+      {
+        name:'CSS3',
+        class:'devicon-css3-plain'
+      },
+      {
+        name:'SASS',
+        class:'devicon-sass-original'
+      },
+      {
+        name:'JavaScript',
+        class:'devicon-javascript-plain'
+      }
+    ],
+    src:`./assets/nygmaPreview.jpg`,
+    description:"A randomly generated maze game where, when completed, the user receives a piece of advice (provided by the AdviceSlip API) based on a keyword they select before starting the game.",
+    repo:`https://github.com/nygma-machine/project-4`,
+    live:`https://nygma.netlify.app`},
+  {
+    name:'The Digital Cinephile',
+    languages:[{
+      name:"HTML5",
+      class:'devicon-html5-plain'
+    },
+    {
+      name:'CSS3',
+      class:'devicon-css3-plain'
+    },
+    {
+      name:'SASS',
+      class:'devicon-sass-original'
+    },
+    {
+      name:'JavaScript',
+      class:'devicon-javascript-plain'
+    }]
+    ,
+    description:`A Movie/TV Show recommendation app, developed through paired programming & built with the TMDB API. A user searches for a movie and/or TV show, then the site  displays a gallery of recommended movies/TV Shows based on their input. The website uses multiple API calls to display each recommendation's poster, official trailer (if available), user rating, and plot overview.`,
+    src:'./assets/cinephilePreview.jpg',
+    repo:`http://github.com/cohort36-Bootcamp-Project-02/theDigitalCinephile/`,
+    live:`https://theDigitalCinephile.netlify.app`
+  },
+  {
+    name:'The Perfect Playlist Procurement Program',
+    languages:[
+      {
+        name:"HTML5",
+        class:'devicon-html5-plain'
+      },
+      {
+        name:'CSS3',
+        class:'devicon-css3-plain'
+      },
+      {
+        name:'SASS',
+        class:'devicon-sass-original'
+      },
+      {
+        name:'JavaScript',
+        class:'devicon-javascript-plain'
+      }
+    ],
+    src:`./assets/playlistPreview.jpg`,
+    description:`A playlist generation app, built using the Deezer API. Users select an artist/band, then a random song of theirs is added to a global 'playlist'. Users can search for bands either by genre, or by searching for their name.`,
+    repo:`https://github.com/chamat96/corey-hamat-project-03`,
+    live:`https://perfect-playlist-procurement.netlify.app`
+  },
+  {
+    name:'Planted',
+    languages:[
+      {
+        name:"HTML5",
+        class:'devicon-html5-plain'
+      },
+      {
+        name:'CSS3',
+        class:'devicon-css3-plain'
+      },
+      {
+        name:'SASS',
+        class:'devicon-sass-original'
+      },
+      {
+        name:'JavaScript',
+        class:'devicon-javascript-plain'
+      }
+    ],
+    src:`./assets/plantedPreview.jpg`,
+    description:"A Multi-Page HTML/SCSS website that was designed based on a client brief/styleguide",
+    repo:`https://github.com/CHamat96/coreyHamatProject01`,
+    live:'https://chamat96.github.io/coreyHamatProject01/index.html'
+  },
 ]
